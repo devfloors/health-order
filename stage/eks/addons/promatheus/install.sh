bcfa7b170ac334449b56147e6d02e324c4359731
devfloors <csh47790@gmail.com>
2023-01-02 21:51:41 +0900

    remove useless

--- stage/eks/addons/promatheus/install.sh
+++
@@ -1,9 +0,0 @@
-aws eks update-kubeconfig --region ap-northeast-2 --name test-cluster
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-kubectl create ns monitoring
-helm install prometheus prometheus-community/kube-prometheus-stack -f "values.yaml" --namespace monitoring
-kubectl port-forward service/prometheus-grafana 3000:80 --namespace monitoring
-
-# id는 admin
-# pw는 prom-operator
