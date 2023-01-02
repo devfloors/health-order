7686b033d1d7ae9d23ce61b6b52597a80db02742
devfloors <csh47790@gmail.com>
2023-01-02 21:50:55 +0900

    remove useless

--- stage/eks/addons/istio-1.16.1/install.sh
+++ stage/eks/addons/istio-1.16.1/install.sh
@@ -1,3 +1,18 @@
+aws eks update-kubeconfig --region ap-northeast-2 --name cluster
+
+# lb controller /stage/eks/lb-controller
+terraform apply
+
+# prometheus 설치
+kubectl apply --server-side -f manifests/setup
+
+kubectl wait \
+        --for condition=Established \
+        --all CustomResourceDefinition \
+        --namespace=monitoring
+
+kubectl apply -f manifests/
+
 curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.16.1 sh -
 
 export PATH="$PATH:/Users/kmov/Documents/tf-k8s-boilerplate/stage/eks/addons/istio-1.16.1/bin"
@@ -8,13 +23,14 @@ kubectl apply -f istio-demo.yaml
 kubectl apply -f samples/addons/kiali.yaml 
 kubectl apply -f samples/addons/jaeger.yaml 
 
-kubectl create ns msa
-kubectl label ns msa istio-injection=enabled
-kubectl get ns msa --show-labels
+kubectl create ns user
+kubectl label ns user istio-injection=enabled
+kubectl get ns user --show-labels
 
 kubectl apply -f prometheus-rbac.yaml
 kubectl apply -f prometheus-operator.yaml
 
+kubectl edit cm -n istio-system kiali  
 ##################################################
     external_services:
       custom_dashboards:
